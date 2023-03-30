88cb569ba3e8c2154e2752f40c77774902375553
Kabucho G <kabuchogeorge3@gmail.com>
2023-03-30 14:19:01 -0700

    list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+from typing import List
 
 """Python annotations"""
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     sums up all the input values in the input list
     """

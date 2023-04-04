cbe3438019e74ad45891c4afcdbead582fd18509
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-04 15:38:40 -0700

    type annotations corrections

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 """ 0. Async Generator """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> asyncio.Task:
+async def async_generator() -> Generator[float, None, None]
     """yield"""
     for i in range(10):
         await asyncio.sleep(1)

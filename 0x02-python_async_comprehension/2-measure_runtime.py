426fbf41be46a204cef825375476c788742e9656
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-04 15:31:40 -0700

    new tasks

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+task 3
+"""
+
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    start_time = time.time()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    return time.time() - start_time
